30bd47b58f63022ea980d8eae60df8c2f4f4d01a
Katherine Gutierrez <katherine.gutierrez@catolica.edu.sv>
2025-02-18 17:14:19 -0600

    Creacion de controladores

---
+++ L01_2022GO650_2022LA605/Controllers/CalificacionesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L01_2022GO650_2022LA605.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class calificacionesController : ControllerBase
+    {
+    }
+}
