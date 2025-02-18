6473ab778cd4f95ab6e2c54d5c9c691c92304420
Katherine Gutierrez <katherine.gutierrez@catolica.edu.sv>
2025-02-18 17:54:40 -0600

    Nuevo commit

--- L01_2022GO650_2022LA605/Controllers/UsuarioController.cs
+++ L01_2022GO650_2022LA605/Controllers/UsuarioController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using L01_2022GO650_2022LA605.Models;
+using Microsoft.Data.SqlClient;
 
 namespace L01_2022GO650_2022LA605.Controllers
 {
@@ -7,5 +10,34 @@ namespace L01_2022GO650_2022LA605.Controllers
     [ApiController]
     public class UsuarioController : ControllerBase
     {
+        private readonly BlogContext _blogContexto;
+
+        public UsuarioController(BlogContext usuarioContexto)
+        {
+            _blogContexto = usuarioContexto;
+        }
+
+        /// <summary>
+        /// EndPoint que retorna el listado de todos los usuarios existentes
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetAll")]
+      
+        public IActionResult Get()
+        {
+            List<Usuarios> listadoUsuarios = (from e in _blogContexto.usuarios
+                                          select e).ToList();
+
+            if (listadoUsuarios.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(listadoUsuarios);
+        }
+
+
+
     }
 }
--- L01_2022GO650_2022LA605/Models/Usuarios.cs
+++ L01_2022GO650_2022LA605/Models/Usuarios.cs
@@ -1,6 +1,15 @@
-﻿namespace L01_2022GO650_2022LA605.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace L01_2022GO650_2022LA605.Models
 {
     public class Usuarios
     {
+        [Key]  // Agregar esta anotación
+        public int UsuarioId { get; set; }
+        public int RolId { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Clave { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
     }
 }
